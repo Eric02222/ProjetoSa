3c61203a3039d7c4e8dc210c25af64f1a416e371
Eric02222 <ericmaraoliveira@gmail.com>
2024-11-14 12:58:43 -0300

    atualizar

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,11 @@
 let cadastro = [];
 
+let logado = [];
+
+let usuario = document.getElementById("usuario");
+let email = document.getElementById("email");
+let senha = document.getElementById("senha");
+
 function carregarCadastro() {
     let dados = localStorage.getItem("cadastro");
     if (dados) {
@@ -11,46 +17,99 @@ function salvarCadastro() {
     localStorage.setItem("cadastro", JSON.stringify(cadastro));
 }
 
-function registro() {
-    let usuario = document.getElementById("usuario").value;
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
+function carregarLogin() {
+    let dadosUser = localStorage.getItem("login");
+    if (dadosUser) {
+        logado = JSON.parse(dadosUser);
+    }
+}
 
-    if (!email || !email || !senha) {
-        document.getElementById("aviso").innerHTML = "verifique se os campos estao preenchidos corretamente";
+function salvarLogin() {
+    localStorage.setItem("login", JSON.stringify(logado));
+}
+
+function registro() {
+    let user = {
+        username: usuario.value,
+        email: email.value,
+        senha: senha.value
+    }
+
+    if (!user.username || !user.email || !user.senha) {
+        document.getElementById("aviso").innerHTML = "Preencha todos os campos";
+    } else if (existe(user.username, user.email) == true) {
+        document.getElementById("aviso").innerHTML = "Usuario já cadastrado!";
     } else {
-        cadastro.push({user: usuario, email: email, senha: senha });
+        cadastro.push(user);
         salvarCadastro();
+        document.getElementById("aviso").innerHTML = "Usuario cadastrado";
         window.location = "login.html";
     }
+
+    usuario.value = null;
+    email.value = null;
+    senha.value = null;
 }
 
 
 function login() {
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
-    let usuarioEncontrado = cadastro.find(user => user.email === email && user.senha === senha);
+    let pos = indexOfByEmail(document.getElementById("email").value);
+    console.log(pos)
+    let nomerUsuario = cadastro[pos].username;
 
-    if (!usuarioEncontrado) {
-        document.getElementById("aviso").innerHTML = "Senha ou email inválido";
-    } else {
-        document.getElementById("aviso").innerHTML = "Login efetuado com sucesso";
+    let userLogado = {
+        username: nomerUsuario,
+        email: document.getElementById("email").value,
+        senha: document.getElementById("senha").value
     }
+
+    if (indexOfByEmail(userLogado.email) == 1) {
+        document.getElementById("aviso").innerHTML = "Você já está logado em sua conta!";
+    } else if (authentica(userLogado.email, userLogado.senha) == true) {
+        document.getElementById("aviso").innerHTML = "Login efetuado com sucesso";
+        logado.push(userLogado);
+        salvarLogin();
+        window.location = "main.html";
+    } else {
+        document.getElementById("aviso").innerHTML = "Senha ou email inválido";
+    }
+    document.getElementById("email").value = null;
+    document.getElementById("senha").value = null;
 }
 
-function resetPass() {
-    let senha = document.getElementById("senha").value;
-    let senhaCo = document.getElementById("senhaCo").value;
-    let usuario = cadastro.find(user => senha)
+/*function logout() {
+    login = false;
+    document.getElementById("aviso").innerHTML = "Logout efetuado com sucesso";
+}*/
 
-    if ( !senha || !senhaCo || senha != senhaCo) {
-        document.getElementById("aviso").innerHTML = "Senhas não conferem";
-    } else {
-        usuario.senha = senha;
-        salvarCadastro();
-        document.getElementById("aviso").innerHTML = "Senha alterada com sucesso";
+
+function existe(username, email) {
+    for (let user of cadastro) {
+        if (user.username == username || user.email == email) {
+            return true;
+        }
     }
+    return false;
+}
 
+function authentica(email, senha) {
+    for (let user of cadastro) {
+        if (user.email == email && user.senha == senha) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function indexOfByEmail(email) {
+    for (let i = 0; i < cadastro.length; i++) {
+        if (cadastro[i].email === email) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 window.onload = carregarCadastro();
+window.onload = carregarLogin();
+
